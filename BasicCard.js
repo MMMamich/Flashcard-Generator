6bb5ded64f6559a3e777c85038fa2129632783a0
MMMamich <MMMamich@gmail.com>
2017-08-25 10:07:58 -0700

    Third Commit

--- BasicCard.js
+++ BasicCard.js
@@ -1,6 +1,14 @@
 var inquirer = require('inquirer');
 
-var ClozeCard = require ('./ClozeCard');
+var toughQuestions = require('./ClozeCard.js');  //Variable has to be named same as function
+
+var clozeQuestions = toughQuestions.clozeQuestions;
+
+var clozeQuestionsPickachu = toughQuestions.clozeQuestionsPikachu;
+
+var partialQuestions = toughQuestions.partialQuestions;
+
+var partialQuestionsPikachu = toughQuestions.partialQuestionsPikachu;
 
 //======================================
 
@@ -12,21 +20,194 @@ function BasicCard (read, front, back) {
     
 }
 
-module.exports = BasicCard;
-
 //======================================
+
+
+//==================================================================================
 
 var q1 = new BasicCard("Question 1: ", "Yoshi", "Yoshi is Peach's Green Dinosaur.");
 var q2 = new BasicCard("Question 2: ", "Piantas", "Piantas Are Inhibantants of Isle Delfino.") ;
-var q3 = new BasicCard("Question 3: ", "1983", "1983 is when Mario Bros. came out.");
+var q3 = new BasicCard("Question 3: ", "1983", "1983 is when Mario Bros came out.");
 var q4 = new BasicCard("Question 4: ", "Toadette", "Toadette is Toads Female Counterpart.");
 var q5 = new BasicCard("Question 5: ", "Odyssey", "Odyssey is the next Mario 3D Platform Game.");
+
+//==================================================================================
 
 var qP1 = new BasicCard("Question 1: ", "Zubat", "Zubat is pokemon number 41.");
 var qP2 = new BasicCard("Question 2: ", "Fairy", "Fair is Super effective against Dark.");
 var qP3 = new BasicCard("Question 3: ", "Ash", "Ash Catchum is the original pokemon trainer.");
 var qP4 = new BasicCard("Question 4: ", "HM05", "HM05 is Flash.");
 var qP5 = new BasicCard("Question 5: ", "Shuckle", "Shuckle is the pokemon with the highest defense.");
+
+//==================================================================================
+
+
+//==================================================================================
+
+function babyQuestions (){
+    
+    inquirer.prompt([
+        
+    {
+      type: 'confirm',  
+      name:"q1", 
+      message: q1.readable + q1.front,  
+      default: true
+    },
+        
+    {
+      type: 'confirm',  
+      name:"q1", 
+      message: q1.readable + q1.back,  
+      default: true
+    },
+        
+    {
+      type: 'confirm',  
+      name:"q2", 
+      message: q2.readable + q2.front,  
+      default: true
+    },
+        
+    {
+      type: 'confirm',  
+      name:"q2", 
+      message: q2.readable + q2.back,  
+      default: true
+    },
+        
+    {
+      type: 'confirm',  
+      name:"q3", 
+      message: q3.readable + q3.front,  
+      default: true
+    },
+        
+    {
+      type: 'confirm',  
+      name:"q3", 
+      message: q3.readable + q3.back,  
+      default: true
+    },
+    
+    {
+      type: 'confirm',  
+      name:"q4", 
+      message: q4.readable + q4.front,  
+      default: true
+    },
+        
+    {
+      type: 'confirm',  
+      name:"q4", 
+      message: q4.readable + q4.back,  
+      default: true
+    },
+        
+    {
+      type: 'confirm',  
+      name:"q5", 
+      message: q5.readable + q5.front,  
+      default: true
+    },
+        
+    {
+      type: 'confirm',  
+      name:"q5", 
+      message: q5.readable + q5.back,  
+      default: true
+    }
+        
+    ]).then(function (answers) {
+    console.log("Good Job...Baby!");
+});   
+}
+
+//==================================================================================
+
+
+//==================================================================================
+
+function pikaQuestions (){
+    
+    inquirer.prompt([
+        
+    {
+      type: 'confirm',  
+      name:"q1", 
+      message: qP1.readable + qP1.front,  
+      default: true
+    },
+        
+    {
+      type: 'confirm',  
+      name:"q1", 
+      message: qP1.readable + qP1.back,  
+      default: true
+    },
+        
+    {
+      type: 'confirm',  
+      name:"q2", 
+      message: qP2.readable + qP2.front,  
+      default: true
+    },
+        
+    {
+      type: 'confirm',  
+      name:"q2", 
+      message: qP2.readable + qP2.back,  
+      default: true
+    },
+        
+    {
+      type: 'confirm',  
+      name:"q3", 
+      message: qP3.readable + qP3.front,  
+      default: true
+    },
+        
+    {
+      type: 'confirm',  
+      name:"q3", 
+      message: qP3.readable + qP3.back,  
+      default: true
+    },
+    
+    {
+      type: 'confirm',  
+      name:"q4", 
+      message: qP4.readable + qP4.front,  
+      default: true
+    },
+        
+    {
+      type: 'confirm',  
+      name:"q4", 
+      message: qP4.readable + qP4.back,  
+      default: true
+    },
+        
+    {
+      type: 'confirm',  
+      name:"q5", 
+      message: qP5.readable + qP5.front,  
+      default: true
+    },
+        
+    {
+      type: 'confirm',  
+      name:"q5", 
+      message: qP5.readable + qP5.back,  
+      default: true
+    }
+        
+    ]).then(function (answers) {
+    console.log("Good Job...Baby!");
+});   
+}
+
+//==================================================================================
 
 inquirer.prompt([
     
@@ -43,7 +224,7 @@ inquirer.prompt([
     type: 'list',
     name: "topicSelect",
     message: "Which Topic Do You Prefer?",
-    choices: ["Mario", "Pokemon"],
+    choices: ["Mario", "Pokemon"]
     
 },
            
@@ -67,31 +248,21 @@ inquirer.prompt([
     when: function(whichRPG){
         return whichRPG.topicSelect === "Pokemon";
     }
-},
-    
-{
-    type: 'confirm', //HOW DO I GET DIFFICULTY - READ BBY, MD, LRG being read to here
-    name:"q1",
-    message: q1.readable + q1.front, //If yes then go to q2 if no then console.log
-    default: true,
-    when: function(quizDiff){
-        return quizDiff.quizType === "Baby";
-        return quizDiff.quizType === "Medium";
-        return quizDiff.quizType === "Hard";
-    }
-},
-    
-{
-    type: 'confirm', //HOW DO I GET DIFFICULTY - READ BBY, MD, LRG being read to here
-    name:"q2",
-    message: q2.readable + q2.front, //If yes then go to q2 if no then console.log
-    default: true
-    
 }
-    
-    
-    
+         
 ]).then(function (inquirerResponse) {
 
+    if (inquirerResponse.quizType === "Baby" && inquirerResponse.topicSelect === "Mario"){ 
+        babyQuestions();
+    } else if (inquirerResponse.quizType === "Medium" && inquirerResponse.topicSelect === "Mario"){
+        clozeQuestions();
+    } else if (inquirerResponse.quizType === "Hard" && inquirerResponse.topicSelect === "Mario"){
+        partialQuestions();
+    } else if (inquirerResponse.quizType === "Baby" && inquirerResponse.topicSelect === "Pokemon"){
+        pikaQuestions();
+    }else if (inquirerResponse.quizType === "Medium" && inquirerResponse.topicSelect === "Pokemon"){
+        clozeQuestionsPickachu();
+    }else if (inquirerResponse.quizType === "Hard" && inquirerResponse.topicSelect === "Pokemon")
+        partialQuestionsPikachu();
 });
 
--- ClozeCard.js
+++ ClozeCard.js
@@ -1,18 +1,7 @@
-var BasicCard = require ('./BasicCard');
+var inquirer = require('inquirer');
 
-//==================================================================
-
-var q1Cloze = new quizCard("Yoshi", "Yoshi is Peach's Green Dinosaur.");
-var q2Cloze = new quizCard("Piantas", "Piantas Are Inhibantants of Isle Delfino.") ;
-var q3Cloze = new quizCard("1983", "1983 is when Mario Bros. came out.");
-var q4Cloze = new quizCard("Toadette", "Toadette is Toads Female Counterpart.");
-var q5Cloze = new quizCard("Odyssey", "Odyssey is the next Mario 3D Platform Game.");
-
-var qP1Cloze = new quizCard("Zubat", "Zubat is pokemon number 41.");
-var qP2Cloze = new quizCard("Fairy", "Fair is Super effective against Dark.");
-var qP3Cloze = new quizCard("Ash Catchum is the original pokemon trainer.");
-var qP4Cloze = new quizCard("HM05", "HM05 is Flash.");
-var qP5Cloze = new quizCard("Shuckle", "Shuckle is the pokemon with the highest defense.");
+var inquiryData = require('./BasicCard.js');
+//===================================================================
 
 function quizCard (cloze, partial) {
     
@@ -21,18 +10,301 @@ function quizCard (cloze, partial) {
     
 }
 
-module.exports = quizCard;
+//==================================================================
 
-//===================================================================
+var q1Cloze = new quizCard("Yoshi is", "...Peach's Green Dinosaur.");
+var q2Cloze = new quizCard("Piantas Are", "...Inhibantants of Isle Delfino.") ;
+var q3Cloze = new quizCard("1983 is when", "...Mario Bros came out.");
+var q4Cloze = new quizCard("Toadette is", "...Toads Female Counterpart.");
+var q5Cloze = new quizCard("Odyssey is", "...the next Mario 3D Platform Game.");
 
-//inquirer.prompt([
-//    
-//    {
-//        
-//        
-//    }
-//    
-//    
-//]).then(function (answers) {
-//    // Use user feedback for... whatever!! 
-//});
+var answerKeyMario = {
+    line1:"\\≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠//",
+    q1: "Yoshi is Peachs Green Dinosaur",
+    q2: "Piantas are Inhibantants of Isle Delfino",
+    q3: "1983 is when Mario Bros came out",
+    q4: "Toadette is Toads Female Counterpart",
+    q5: "Odyssey is the next Mario 3D Platform Game",
+    line2:"\\≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠//"
+}
+//==================================================================
+
+var q1PikaCloze = new quizCard("Zubat is pokemon number", "...pokemon number 41.");
+var q2PikaCloze = new quizCard("Fairy is", "...Super Effective against Dark.");
+var q3PikaCloze = new quizCard("Ash Catchum is", "...the original pokemon trainer.");
+var q4PikaCloze = new quizCard("HM05 is", "...is Flash.");
+var q5PikaCloze = new quizCard("Shuckle is the", "...pokemon with the highest defense.");
+
+var answerKeyPika = {
+    
+    line1:"\\≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠//",
+    q1:"Zubat is pokemon number 41",
+    q2: "Fairy is Super Effective against Dark", 
+    q3: "Ash Catchum is the original pokemon trainer",
+    q4: "HM05 is Flash",
+    q5: "Odyssey is the next Mario 3D Platform Game",
+    line2:"\\≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠≠//",
+}
+
+//==================================================================
+
+
+//============================================================================================================
+exports.clozeQuestions = function() {
+
+
+    inquirer.prompt([
+    
+    {
+        
+        type: 'confirm',
+        name: "instructions",
+        message: "You'll Be Given the Answer, finish the rest of the statement.  Understand?", //If No go back to quizType
+        default: true 
+        
+    },
+        
+        
+    {
+        type: "input",
+        message: q1Cloze.cloze,
+        name: "q1Clz"
+        
+    },
+        
+    {
+        type: "input",
+        message: q2Cloze.cloze,
+        name: "q2Clz"
+        
+    },
+        
+    {
+        type: "input",
+        message: q3Cloze.cloze,
+        name: "q3Clz"
+        
+    },
+        
+    {
+        type: "input",
+        message: q4Cloze.cloze,
+        name: "q4Clz"
+        
+    },
+    
+    {
+        type: "input",
+        message: q5Cloze.cloze,
+        name: "q5Clz"
+        
+    },
+    
+    
+]).then(function (cloze) {
+        
+    for (key in answerKeyMario){
+        console.log(answerKeyMario[key]);
+    }
+    
+});
+    
+}
+
+//============================================================================================================
+
+
+//============================================================================================================
+exports.clozeQuestionsPikachu = function() {
+
+
+    inquirer.prompt([
+    
+    {
+        
+        type: 'confirm',
+        name: "instructions",
+        message: "You'll Be Given the Answer, finish the rest of the statement.  Understand?", //If No go back to quizType
+        default: true 
+        
+    },
+        
+        
+    {
+        type: "input",
+        message: q1PikaCloze.cloze,
+        name: "q1Clz"
+        
+    },
+        
+    {
+        type: "input",
+        message: q2PikaCloze.cloze,
+        name: "q2Clz"
+        
+    },
+        
+    {
+        type: "input",
+        message: q3PikaCloze.cloze,
+        name: "q3Clz"
+        
+    },
+        
+    {
+        type: "input",
+        message: q4PikaCloze.cloze,
+        name: "q4Clz"
+        
+    },
+    
+    {
+        type: "input",
+        message: q5PikaCloze.cloze,
+        name: "q5Clz"
+        
+    },
+    
+    
+]).then(function (cloze) {
+    for (key in answerKeyPika){
+        console.log(answerKeyPika[key]);
+    }
+    
+});
+    
+}
+
+//============================================================================================================
+
+
+//============================================================================================================
+
+exports.partialQuestions = function () { 
+
+inquirer.prompt([
+
+    {
+        type: 'confirm',
+        message: "You'll Be Given thee Statement, finish the rest of the Answer.  Understand?", //If No go back to quizType
+        name: "instructions",
+        default: true 
+    
+    },
+    
+    {
+        
+        type: "input",
+        message: q1Cloze.partial,
+        name: "q1Prt"
+        
+    },
+    
+    {
+        
+        type: "input",
+        message: q2Cloze.partial,
+        name: "q2Prt"
+        
+    },
+    
+    {
+        
+        type: "input",
+        message: q3Cloze.partial,
+        name: "q3Prt"
+        
+    },
+    
+    {
+        
+        type: "input",
+        message: q4Cloze.partial,
+        name: "q4Prt"
+        
+    },
+    
+    {
+        
+        type: "input",
+        message: q5Cloze.partial,
+        name: "q5Prt"
+        
+    }
+
+
+]).then(function (partial) {
+    for (key in answerKeyMario){
+        console.log(answerKeyMario[key]);
+    }
+});
+
+};
+
+//============================================================================================================
+
+
+//============================================================================================================
+
+exports.partialQuestionsPikachu = function () { 
+
+inquirer.prompt([
+
+    {
+        type: 'confirm',
+        message: "You'll Be Given thee Statement, finish the rest of the Answer.  Understand?", //If No go back to quizType
+        name: "instructions",
+        default: true 
+    
+    },
+    
+    {
+        
+        type: "input",
+        message: q1PikaCloze.partial,
+        name: "q1Prt"
+        
+    },
+    
+    {
+        
+        type: "input",
+        message: q2PikaCloze.partial,
+        name: "q2Prt"
+        
+    },
+    
+    {
+        
+        type: "input",
+        message: q3PikaCloze.partial,
+        name: "q3Prt"
+        
+    },
+    
+    {
+        
+        type: "input",
+        message: q4PikaCloze.partial,
+        name: "q4Prt"
+        
+    },
+    
+    {
+        
+        type: "input",
+        message: q5PikaCloze.partial,
+        name: "q5Prt"
+        
+    }
+
+
+]).then(function (partial) {
+    for (key in answerKeyPika){
+        console.log(answerKeyPika[key]);
+    }
+});
+
+};
+
+//============================================================================================================
